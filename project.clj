62a5aff9149f3f2d1cf215d4218fa171242106e3
thom <chiovolonit@gmail.com>
2012-03-13 14:11:43 -0400

    might have fixed stack overflow issues

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject unlambda-clj "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject unlambda-clj "0.1.0-SNAPSHOT"
+  :description "Unlambda in clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :main unlambda-clj.core)
--- src/unlambda_clj/core.clj
+++ src/unlambda_clj/core.clj
@@ -3,46 +3,40 @@
 
 (def cur-char (atom nil))
 
-(defn- bounce
-  "like trampoline, but evaluates to nil, and doesn't ever call the result of the function given to it."
-  ([f] (trampoline #(do (f) nil)))
-  ([f & args] (bounce #(apply f args))))
-
 (defn evaluate
   ([program] (evaluate program identity))
   ([program result-callback]
      (letfn [(app [[func closure] arg cont]
- ;              (println (str "ARGS TO APP: " [[func closure] arg]))
+;               (println (str "ARGS TO APP: " [[func closure] arg]))
                (case func
-                 "." (do (.write *out* closure) (bounce cont arg))
-                 "r" (do (.write *out* "\n") (bounce cont arg))
-                 "i" (bounce cont arg)
-                 "k" (bounce cont ["k1" arg])
-                 "k1" (bounce cont closure)
-                 "s" (bounce cont ["s1" arg])
-                 "s1" (bounce cont ["s2" [closure, arg]])
-                 "s2" (let [[f1 f2] closure] (ev ["`" [["`" [f1 arg]] ["`" [f2 arg]]]] cont))
-                 "v" (bounce cont ["v", nil])
-                 "d1" (ev ["`" [closure, arg]], #(bounce cont %))
+                 "." (do (.write *out* closure) #(cont arg))
+                 "r" (do (.write *out* "\n") #(cont arg))
+                 "i" #(cont arg)
+                 "k" #(cont ["k1" arg])
+                 "k1" #(cont closure)
+                 "s" #(cont ["s1" arg])
+                 "s1" #(cont ["s2" [closure, arg]])
+                 "s2" (let [[f1 f2] closure] #(ev ["`" [["`" [f1 arg]] ["`" [f2 arg]]]] cont))
+                 "v" #(cont ["v", nil])
+                 "d1" #(ev ["`" [closure, arg]] cont)
                  "e" (result-callback arg)
-                 "@" (do (reset! cur-char (.readChar *in*))
-                         (ev ["`" [arg [(if @cur-char "i" "v") nil]]] cont))
-                 "|" (ev ["`" [arg (if @cur-char ["." @cur-char] ["v" nil])]] cont)
-                 "?" (ev ["`" [arg [(if (= @cur-char closure) "i" "v") nil]]] cont)
-                 "c" (ev ["`" [arg ["c1" cont]]] cont)
-                 "c1" (bounce closure arg)))
+                 "@" (do (reset! cur-char (.read *in*))
+                         #(ev ["`" [arg [(if @cur-char "i" "v") nil]]] cont))
+                 "|" #(ev ["`" [arg (if @cur-char ["." @cur-char] ["v" nil])]] cont)
+                 "?" #(ev ["`" [arg [(if (= @cur-char closure) "i" "v") nil]]] cont)
+                 "c" #(ev ["`" [arg ["c1" cont]]] cont)
+                 "c1" #(closure arg)))
              (ev [[func closure] cont]
 ;               (println (str "ARGS TO EV: " [func closure]))
                (if-not (= func "`")
-                 (bounce cont [func closure])
+                 (trampoline cont [func closure])
                  (let [[func arg] closure]
                    (recur func
                      (fn [op]
-;                       (println (str "OP IS " op))
                        (if (= "d" (first op))
-                         (bounce cont ["d1" arg])
-                         (ev arg (fn [earg]
-                                   (app op earg (fn [res] (bounce cont res)))))))))))]
+                         #(cont ["d1" arg])
+                         #(ev arg (fn [earg]
+                                    (app op earg cont)))))))))]
        (ev program result-callback))))
 
 (defn parse [program]
@@ -60,6 +54,6 @@
       (parse-out))))
 
 (defn interpret [code] (evaluate (parse code)))
-
- ;```s``sii`ki``s``s`ks ``s``s`ks``s`k`s`kr ``s`k`si``s`k`s`k
- ;`d````````````.H.e.l.l.o.,. .w.o.r.l.d.! k k `k``s``s`ksk`k.*
+(defn -main [& args]
+  
+  (interpret "`r```si`k``s ``s`kk `si ``s``si`k ``s`k`s`k ``sk ``sr`k.* i r``si``si``si``si``si``si``si``si``si`k`ki"))
